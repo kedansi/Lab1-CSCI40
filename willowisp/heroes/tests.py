de41f43916290c5c1004ebdb58e30f99219f8377
Kevin Sibug <38392797+kedansi@users.noreply.github.com>
2020-03-07 19:30:42 +0800

    Added unit tests for each view

--- willowisp/heroes/tests.py
+++ willowisp/heroes/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HeroesTest(TestCase):
+    def test_can_get_create_template(self):
+        response = self.client.get('/heroes/')
+        self.assertTemplateUsed(response, 'heroes.html')
+class CloudTest(TestCase):
+    def test_can_get_create_template(self):
+        response = self.client.get('/hero/cloud/')
+        self.assertTemplateUsed(response, 'detail_cloud.html')
+class JesterTest(TestCase):
+    def test_can_get_create_template(self):
+        response = self.client.get('/hero/jester/')
+        self.assertTemplateUsed(response, 'detail_jester.html')
+class SunfloweyTest(TestCase):
+    def test_can_get_create_template(self):
+        response = self.client.get('/hero/sunflowey/')
+        self.assertTemplateUsed(response, 'detail_sunflowey.html')
