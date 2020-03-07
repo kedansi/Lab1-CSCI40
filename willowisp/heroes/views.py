c035d8765bf6ed9134c687d1bbb6c46f0f14b678
Kevin Sibug <38392797+kedansi@users.noreply.github.com>
2020-03-07 19:59:31 +0800

    Updated views.py

--- willowisp/heroes/views.py
+++ willowisp/heroes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from django.views.generic.base import TemplateView
 # Create your views here.
+
+class HeroesView(TemplateView):
+    template_name = "heroes.html"
+    
+class CloudView(TemplateView):
+    template_name = "detail_cloud.html"
+
+class JesterView(TemplateView):
+    template_name = "detail_jester.html"
+
+class SunfloweyView(TemplateView):
+    template_name = "detail_sunflowey.html"
